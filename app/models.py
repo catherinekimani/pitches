8ffb2cff6cc87cfdabc47095dc2c0d417ce13b83
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-07 22:43:09 +0300

    Feat: Update models

--- app/models.py
+++ app/models.py
@@ -1,20 +1,35 @@
+from datetime import datetime
 from . import db
+from datetime import datetime
 
 class User(db.Model):
-    pass
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable=False)
 
 class Pitch(db.Model):
-    pass
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(30),nullable=False)
+    description = db.Column(db.Text, nullable=False )
+    user = db.Column(db.String, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class Comments(db.Model):
-    pass 
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key = True)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Upvotes(db.Model):
-    pass
-
+    __tablename__ = 'upvotes'
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
 class Downvotes(db.Model):
-    pass
-
+    __tablename__ = 'downvotes'
+    id = db.Column(db.Integer,primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         return f'User {self.username}'
