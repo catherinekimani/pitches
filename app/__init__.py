8b32ea97fd11c464972462e9fcd0917d000e3fe5
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-09 02:25:26 +0300

    Feat: Import uploadSet-to display user profile

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     
 # initializing application
@@ -33,6 +35,9 @@ def create_app(config_name):
 
 # creating app configurations
     app.config.from_object(config_options[config_name])
+    
+# upload set configuration
+    configure_uploads(app,photos)
     
     return app
     
