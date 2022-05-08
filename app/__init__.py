b44650df052471590da5efed9e221e761d3e9a20
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-08 03:58:46 +0300

    Style: Initialize Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
+
 def create_app(config_name):
     
 # initializing application
@@ -14,7 +17,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-# initialize flask extension
+# initialize flask extensions
+    bootstrap.init_app(app)
     db.init_app(app)
 
 # creating app configurations
