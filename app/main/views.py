fb7628b33a822143050ccd58f049e33e730a89a9
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-08 04:52:53 +0300

    Feat: Create route decorater

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from . import main
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    return render_template('index.html')
