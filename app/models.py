585740228bdf16d34224840b75245ce81b7a76f7
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-08 14:37:35 +0300

    Feat: Create relationships

--- app/models.py
+++ app/models.py
@@ -1,30 +1,49 @@
 from datetime import datetime
 from . import db
-from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(20), unique = True, nullable=False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # create relationship
+    pitches = db.relationship('Pitch',backref = 'pitch',lazy = 'dynamic')
+    comments = db.relationship('Comments',backref = 'pitch', lazy = 'dynamic')
+    upvotes = db.relationship('Upvotes', backref = 'pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvotes', backref = 'pitch', lazy = 'dynamic')
+    
+    def __repr__(self):
+        return f'User {self.username}'
+    
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(30),nullable=False)
     description = db.Column(db.Text, nullable=False )
     user = db.Column(db.String, nullable=False)
+    category = db.Column(db.String(150), index = True,nullable = False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return f'User {self.category}'
+    
 class Comments(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer,primary_key = True)
     text = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
+    
+    def __repr__(self):
+        return f'User {self.text}'
+    
 class Upvotes(db.Model):
     __tablename__ = 'upvotes'
     id = db.Column(db.Integer,primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return f'User {self.user_id}'
     
 class Downvotes(db.Model):
     __tablename__ = 'downvotes'
@@ -32,4 +51,4 @@ class Downvotes(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User {self.user_id}'
