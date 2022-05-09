26dbd8a59c44a373037c3965223503059456010e
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-09 22:47:24 +0300

    Feat: Add register and login views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,13 @@
 from flask import render_template,redirect,url_for,request,flash
 from .import auth
 from .. models import User
-from flask_login import login_user
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
+from ..email import mail_message
 
 
-@auth.route('/login')
+@auth.route('/login',methods=["GET","POST"])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
@@ -21,15 +21,17 @@ def login():
     title = "Pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-@auth.route('/register',methods = ["GET","POST"])
+@auth.route('/register',methods = ['GET','POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+        
+        mail_message('Welcome to Pitches','email/welcome_user',user.email,user=user)
         return redirect(url_for('auth.login'))
-    title = "New Account"
+    # title = "New Account"
     return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/logout')
