f9e89b5328525aa3555f8b7794dc5a0c77a124c2
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-09 22:23:20 +0300

    Feat: Add test command - to run tests

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User,Pitch
 from flask_migrate import Migrate,MigrateCommand
 
 # create app instance
@@ -11,10 +11,16 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Pitch = Pitch )
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
