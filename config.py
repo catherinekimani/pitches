2d6eb4c79941487354a8ac178495977bc5caf36a
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-09 22:21:16 +0300

    Add slight changes

--- config.py
+++ config.py
@@ -5,7 +5,14 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:wambui@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = 'catherinekimani'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class ProdConfig(Config):
     '''
