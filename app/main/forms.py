a699eb96e44202d4c323e669605dbf3d4483e6bb
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-11 08:19:41 +0300

    Feat: Create Pitch and comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import DataRequired
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
     submit = SubmitField('Submit')
+    
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Inspirational','Inspirational'),('Love','Love'),('Educational','Educational')],validators=[DataRequired()])
+    text = TextAreaField('Share Your Pitch', validators=[DataRequired()])
+    submit = SubmitField('Pitch')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[DataRequired()])
+    submit = SubmitField('Comment')
