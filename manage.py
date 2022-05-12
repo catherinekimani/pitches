432a4fd2a7735c516d7d0b8823e56c61c378b7a6
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-12 04:15:58 +0300

    Add imports

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Pitch
+from app.models import User,Pitch,Comments,Upvotes,Downvotes
 from flask_migrate import Migrate,MigrateCommand
 
 # create app instance
@@ -11,7 +11,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch = Pitch )
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Comments=Comments,Upvote=Upvotes,Downvote=Downvotes )
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
