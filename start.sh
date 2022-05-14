e9c11d953fccd4455ea17e5bc5dbe2950ea93069
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-14 11:07:38 +0300

    Deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export MAIL_USERNAME='catherine.kimani@student.moringaschool.com'
 export MAIL_PASSWORD='kaykatekruz1'
+export DATABASE_URL='postgres://xsthcqfgllwtfe:9b9019ceadb28f4ea69f450a7536cf004ea30155d7d147ca4c6d8b20e298563a@ec2-44-196-223-128.compute-1.amazonaws.com:5432/d625updfh3b9ia'
 python3.manage.py server
